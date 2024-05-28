97330c02d8a588334a53a3efff01b8d2216df4c5
lihuimiao <lihuimiao1127@126.com>
2024-05-28 17:56:26 +0800

    12

--- app/src/main/java/com/lee/jni/MainActivity.kt
+++ app/src/main/java/com/lee/jni/MainActivity.kt
@@ -28,10 +28,10 @@ class MainActivity : AppCompatActivity() {
         val p = JNIUtils().createPerson("小丽",30)
         println("LEE-> p.name : ${p.name},p.age:${p.age}")
     }
-    /** 测试git000000案例三等奖发拉升阶段111111阿斯蒂芬 切换到51111111111156dev分支*/
+    /** 测试git000000案例三等奖发拉升阶段111111阿斯蒂芬 切换到41111111111156dev分支*/
     
     private fun testJNI() {
-        /** 调用jni方法并返回求和结果*/
+        /** 调用jni方法并返回求和结果1*/
         val sum = JNIUtils().calculate(100, 200)
         println("LEE-> sum :$sum")
         binding.sampleText.text = sum.toString()
