68afbf5ccaa60eeef7ffe8c0414ff71ee27a6c20
lihuimiao <lihuimiao1127@126.com>
2024-05-28 16:50:07 +0800

    修改了main代码000....12312334阿萨德

--- app/src/main/java/com/lee/jni/MainActivity.kt
+++ app/src/main/java/com/lee/jni/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         val p = JNIUtils().createPerson("小丽",30)
         println("LEE-> p.name : ${p.name},p.age:${p.age}")
     }
-    /** 测试git000000案例三等奖发拉升阶段111111*/
+    /** 测试git000000案例三等奖发拉升阶段111111阿斯蒂芬*/
     
     private fun testJNI() {
         /** 调用jni方法并返回求和结果*/
